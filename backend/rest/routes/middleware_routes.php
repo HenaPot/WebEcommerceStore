ce966f3b7fe78cec0a3f0696addeacca12197a66
HenaPot <hena.potogija@stu.ibu.edu.ba>
2025-03-11 15:11:06 +0100

    not working middleware

--- backend/rest/routes/middleware_routes.php
+++ backend/rest/routes/middleware_routes.php
@@ -5,41 +5,6 @@ use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
 
-// Flight::route('/*', function(){
-//     $req_method = Flight::request()->method;
-//     $req_url = Flight::request()->url;
-//     if($req_method == 'POST' && $req_url == "/login"){
-//         return TRUE;
-//     }
-//     if($req_method == 'GET' && $req_url == "/login"){
-//         return TRUE;
-//     }
-//     if($req_method == 'GET' && $req_url == "/register"){
-//         return TRUE;
-//     }
-//     if($req_method == 'POST' && $req_url == "/users"){
-//         return TRUE;
-//     }
-//     try{
-//         $token = Flight::request()->getHeader('Authentication');
-//         if(!$token){
-//             Flight::halt(401, 'Token not provided');
-//         }
-        
-//         $decoded_token = JWT::decode($token, new Key(Config::JWT_SECRET(), 'HS256'));
-//         error_log("Decoded Token: " . print_r($decoded_token, true));
-
-//         Flight::set('user', $decoded_token->user->id);
-//         Flight::set('jwt_token', $token);
-
-//         return TRUE;
-//     } catch(Exception $e){
-//         error_log("JWT Error: " . $e->getMessage());
-//         Flight::halt(401, $e->getMessage());
-//     }
-// });
-
-
 Flight::route('/*', function() {
     if(
         strpos(Flight::request()->url, '/login') === 0 ||
@@ -53,11 +18,11 @@ Flight::route('/*', function() {
                 Flight::halt(401, "Missing authentication header");
 
             $decoded_token = JWT::decode($token, new Key(Config::JWT_SECRET(), 'HS256'));
-
-            Flight::set('user', $decoded_token->user);
+            
+            Flight::set('user', 999);
             Flight::set('jwt_token', $token);
             return TRUE;
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             Flight::halt(401, $e->getMessage());
         }
     }
